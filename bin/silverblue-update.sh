a5f910617dd127d690415a0453429c7c4c33eadf
Tony Walker <tony.walker.iu@gmail.com>
2022-03-11 13:10:09 -0500

    Fixed a few small problems. Minor code clean-up.

--- bin/silverblue-update.sh
+++ bin/silverblue-update.sh
@@ -1,26 +1,40 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 #
-# silverblue-update
-# Copyright (c) 2022 Tony Walker
+# MIT License
+#
+# Copyright (c) 2022  Tony Walker
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
 #
 
-/usr/bin/install -o root -g root -m 755 -d /var/run/silverblue-update
-if [ $? -ne 0 ]
-then
+if ! /usr/bin/install -o root -g root -m 755 -d /var/run/silverblue-update; then
     logger "silverblue-update: Could not create /var/run/silverblue-update"
     exit 1
 fi
 
-/usr/bin/flatpak update -y --system --noninteractive >/var/run/silverblue-update/flatpak.log 2>&1
-if [ $? -ne 0 ]
-then
+if ! /usr/bin/flatpak update -y --system --noninteractive >/var/run/silverblue-update/flatpak.log 2>&1; then
     logger "silverblue-update: Flatpak update failed"
     exit 1
 fi
 
-/usr/bin/rpm-ostree update >/var/run/silverblue-update/rpm-ostree.log 2>&1
-if [ $? -ne 0 ]
-then
+if ! /usr/bin/rpm-ostree update >/var/run/silverblue-update/rpm-ostree.log 2>&1; then
     logger "silverblue-update: rpm-ostree update failed"
     exit 1
 fi
--- install.sh
+++ install.sh
@@ -1,30 +1,49 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 #
-# silverblue-update
-# Copyright (c) 2022 Tony Walker
+# MIT License
+#
+# Copyright (c) 2022  Tony Walker
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
 #
 
 echo "Installing the Silverblue Daily Update Tool"
 
-if [ $(whoami) != "root" ]
-then
+if [ $(whoami) != "root" ]; then
     echo "* ERROR: Must be root or use sudo."
     exit 1
 fi
 
-echo "* Disabling rpm-ostree-countme..."
-systemctl stop    rpm-ostree-countme.timer || exit 1
-systemctl disable rpm-ostree-countme.timer || exit 1
+echo "* Disabling rpm-ostreed-automatic.timer..."
+systemctl stop rpm-ostreed-automatic.timer || exit 1
+systemctl disable rpm-ostreed-automatic.timer || exit 1
 
 echo "* Installing..."
-install -o root -g root -m 644 units/silverblue-update.timer   /etc/systemd/system/ || exit 1
+install -o root -g root -m 644 units/silverblue-update.timer /etc/systemd/system/ || exit 1
 install -o root -g root -m 644 units/silverblue-update.service /etc/systemd/system/ || exit 1
-install -o root -g root -m 755 bin/silverblue-update.sh        /usr/local/bin/      || exit 1
-install -o root -g root -m 755 -d /var/run/silverblue-update                        || exit 1
+install -o root -g root -m 755 bin/silverblue-update.sh /usr/local/bin/ || exit 1
+install -o root -g root -m 755 -d /var/run/silverblue-update || exit 1
 
 echo "* Starting..."
 systemctl enable silverblue-update.timer || exit 1
-systemctl start  silverblue-update.timer || exit 1
+systemctl start silverblue-update.timer || exit 1
 
 echo "* Done!"
 exit 0
